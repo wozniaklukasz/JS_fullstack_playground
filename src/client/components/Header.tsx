d9ab088a62bf539e967885de832580f65ab94101
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2020-10-22 16:09:51 +0200

    some todo

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -58,7 +58,6 @@ const Header = () => {
           <Link to="/admins" className="nav-link">Admins</Link>
         </Nav>
         <Nav className="justify-content-end">
-          <Nav.Link href="/graphiql" target="graphiql">GraphiQL</Nav.Link>
           {authButton(currentUser)}
         </Nav>
       </Container>
--- src/client/pages/HomePage.tsx
+++ src/client/pages/HomePage.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import {Button} from 'react-bootstrap';
 
 const HomePage = () => {
   return (
     <div>
-      <div>HOME 2</div>
-      <button onClick={() => console.log("click")}>click</button>
+      <h1>JS Playground</h1>
+
+      <h4>Links:</h4>
+      <a href="https://github.com/wozniaklukasz/JS_fullstack_playground" target="projectGhRepo">
+        <Button variant="outline-dark">
+          GH Repo
+        </Button>
+      </a>
+      <a href="/graphql" target="graphql">
+        <Button variant="outline-danger">
+          GraphQL
+        </Button>
+      </a>
+
+      <h4>TODO (v.1):</h4>
+      <ul>
+        <li>CRUD users</li>
+        <li>Admin can change users role</li>
+        <li>Docker compose with deploy</li>
+        <li>Tests</li>
+        <li>CSS grid (keep it simple!)</li>
+        <li>GH CI/CD</li>
+        <li>Readme</li>
+      </ul>
     </div>
   );
 };
